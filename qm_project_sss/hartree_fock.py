fae6df7b4a74c2900b487b0ec0fd157ce6530487
Saumil Chheda <chhed008@umn.edu>
2019-07-25 20:35:09 -0500

    Updated the Hartree_fock class

--- qm_project_sss/hartree_fock.py
+++ qm_project_sss/hartree_fock.py
@@ -14,11 +14,13 @@ class Hartree_Fock:
 
         self.ndof = len(self.atomic_coordiantes) * self.gas_model.orbitals_per_atom
 
+        self.chi_tensor = self.calculate_chi_tensor()
+
+        self.potential_vector = self.calculate_potential_vector()
+
         self.interaction_matrix = self.calculate_interaction_matrix()
 
-        self.density_matrix = self.calculate_density_matrix()
-
-        self.chi_tensor = self.calculate_chi_tensor()
+        self.density_matrix = self.calculate_atomic_density_matrix()
 
         self.hamiltonian_matrix = self.calculate_hamiltonian_matrix()
 
@@ -111,7 +113,7 @@ class Hartree_Fock:
 
     def calculate_potential_vector(self):
 
-        potential_vector = np.zeros(self.ndof)
+        self.potential_vector = np.zeros(self.ndof)
 
         for p in range(self.ndof):
 
@@ -121,7 +123,171 @@ class Hartree_Fock:
 
                 if atom_i != self.gas_model.atom(p):
 
-                    potential_vector[p] += ( self.pseudopotential_energy(self.gas_model.orb(p), r_pi) -
+                    self.potential_vector[p] += ( self.pseudopotential_energy(self.gas_model.orb(p), r_pi) -
                                         self.gas_model.ionic_charge * self.coulomb_energy(self.gas_model.orb(p), 's', r_pi) )
 
-        return potential_vector
+        return self.potential_vector
+
+
+    def calculate_interaction_matrix(self):
+
+        interaction_matrix = np.zeros ((self.ndof,self.ndof))
+
+        for p in range(self.ndof):
+
+            for q in range(self.ndof):
+
+                if self.gas_model.atom(p) != self.gas_model.atom(q):
+
+                    r_pq = self.atomic_coordinates[self.gas_model.atom(p)] - self.atomic_coordinates[self.gas_model.atom(q)]
+
+                    interaction_matrix[p,q] = self.coulomb_energy(self.gas_model.orb(p),self.gas_model.orb(q), r_pq)
+
+                if p==q and self.gas_model.orb(p)=='s':
+
+                    interaction_matrix[p,q] = self.gas_model.model_parameters['coulomb_s']
+
+                if p==q and self.gas_model.orb(p) in self.gas_model.p_orbitals:
+
+                    interaction_matrix[p,q] = self.gas_model.model_parameters['coulomb_p']
+
+        return interaction_matrix
+
+
+    def chi_on_atom(self, o1, o2, o3):
+
+        if o1 == o2 and o3 =='s':
+
+            return 1.0
+
+        if o1 == o3 and o3 in self.gas_model.p_orbitals and o2 =='s':
+
+            return self.gas_model.model_parameters['dipole']
+
+        if o2 == o3 and o3 in self.gas_model.p_orbitals and o1 =='s':
+
+            return self.gas_model.model_parameters['dipole']
+
+        return 0.0
+
+
+    def calculate_chi_tensor(self):
+
+        chi_tensor = np.zeros( (self.ndof, self.ndof, self.ndof) )
+
+        for p in range(self.ndof):
+
+            for orb_q in self.gas_model.orbital_types:
+
+                q = self.gas_model.ao_index(self.gas_model.atom(p), orb_q)
+
+                for orb_r in self.gas_model.orbital_types:
+
+                    r = self.gas_model.ao_index(self.gas_model.atom(p), orb_r)
+
+                    chi_tensor[p,q,r] = self.chi_on_atom(self.gas_model.orb(p), self.gas_model.orb(q), self.gas_model.orb(r))
+
+        return chi_tensor
+
+
+    def calculate_hamiltonian_matrix(self):
+
+        hamiltonian_matrix = np.zeros( (self.ndof, self.ndof) )
+
+        potential_vector = self.calculate_potential_vector()
+
+        for p in range(self.ndof):
+
+            for q in range(self.ndof):
+
+                if self.gas_model.atom(p) != self.gas_model.atom(q):
+
+                    r_pq = self.atomic_coordinates[self.gas_model.atom(p)] - self.atomic_coordinates[self.gas_model.atom(q)]
+
+                    hamiltonian_matrix[p,q] = self.hopping_energy(self.gas_model.orb(p), self.gas_model.orb(q), r_pq)
+
+                if self.gas_model.atom(p) == self.gas_model.atom(q):
+
+                    if p == q and self.gas_model.orb(p) == 's':
+
+                        hamiltonian_matrix[p,q] += self.gas_model.model_parameters['energy_s']
+
+                    if p == q and self.gas_model.orb(p) in self.gas_model.p_orbitals:
+
+                        hamiltonian_matrix[p,q] += self.gas_model.model_parameters['energy_p']
+
+                    for orb_r in self.gas_model.orbital_types:
+
+                        r = self.gas_model.ao_index(self.gas_model.atom(p), orb_r)
+
+                        hamiltonian_matrix[p,q] += ( self.chi_on_atom(self.gas_model.orb(p), self.gas_model.orb(q), orb_r)
+                                                 * self.potential_vector[r] )
+
+        return hamiltonian_matrix
+
+
+    def calculate_atomic_density_matrix(self):
+
+        density_matrix = np.zeros( (self.ndof, self.ndof) )
+
+        for p in range(self.ndof):
+
+            density_matrix[p,p] = self.gas_model.orbital_occupation[self.gas_model.orb(p)]
+
+        return density_matrix
+
+
+    def calculate_fock_matrix(self, old_density_matrix):
+
+        fock_matrix = self.hamiltonian_matrix.copy()
+
+        fock_matrix += 2.0*np.einsum('pqt,rsu,tu,rs->pq', self.chi_tensor, self.chi_tensor, self.interaction_matrix, old_density_matrix , optimize=True)
+
+        fock_matrix -= np.einsum('rqt,psu,tu,rs->pq', self.chi_tensor, self.chi_tensor, self.interaction_matrix, old_density_matrix, optimize=True)
+
+        return fock_matrix
+
+
+    def calculate_density_matrix(self):
+
+        num_occ = (self.gas_model.ionic_charge//2) * np.size(self.fock_matrix,0) // orbitals_per_atom
+
+        orbital_energy, orbital_matrix = np.linalg.eigh(self.fock_matrix)
+
+        occupied_matrix = orbital_matrix[:,:num_occ]
+
+        density_matrix = occupied_matrix @ occupied_matrix.T
+
+        return density_matrix
+
+
+    def scf_cycle( self, max_scf_iterations = 100, mixing_fraction = 0.25, convergence_tolerance = 1e-4):
+
+        old_density_matrix = self.density_matrix.copy()
+
+        for iteration in range(max_scf_iterations):
+
+            self.fock_matrix = self.calculate_fock_matrix(old_density_matrix)
+
+            self.density_matrix = calculate_density_matrix()
+
+            error_norm = np.linalg.norm( old_density_matrix - self.density_matrix)
+
+            print(iteration, error_norm)
+
+            if error_norm < convergence_tolerance:
+
+                return self.density_matrix, self.fock_matrix
+
+            old_density_matrix = (mixing_fraction * self.density_matrix + (1-mixing_fraction) * old_density_matrix )
+
+        print("Warning: SCF Cycle did not converge")
+
+        return self.density_matrix, self.fock_matrix
+
+
+    def calculate_energy_scf(self):
+
+        energy_scf = np.einsum('pq,pq', self.hamiltonian_matrix + self.fock_matrix, self.density_matrix)
+
+        return energy_scf
